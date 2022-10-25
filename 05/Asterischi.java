72d9237d0739949249edb2ca427aa874d9674a1f
Ela <64909186+Ela17@users.noreply.github.com>
2022-10-25 14:08:11 +0200

    Alcune modifiche
    modificati:
    - lab2>SommaInteri
    - lab5>Asterischi

--- 05/Asterischi.java
+++ 05/Asterischi.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class Asterischi {
 
-    public static void rettangoloAsterischi() {
-        Scanner sc = new Scanner(System.in);
-        int l, h;
-        System.out.print("Inserire altezza rettangolo: ");
-        h = sc.nextInt();
-        System.out.print("Inserire larghezza rettangolo: ");
-        l = sc.nextInt();
-
+    public static void rettangoloAsterischi(int l, int h) {
         for(int i = 0; i<h; i++){
             for(int j = 0; j<l; j++)
                 System.out.print("*");
@@ -17,7 +10,16 @@ public class Asterischi {
         }
 
     }
+    
     public static void main(String[] args) {
-        rettangoloAsterischi();
+        Scanner sc = new Scanner(System.in);
+        int l, h;
+
+        System.out.print("Inserire altezza rettangolo: ");
+        h = sc.nextInt();
+        System.out.print("Inserire larghezza rettangolo: ");
+        l = sc.nextInt();
+
+        rettangoloAsterischi(l,h);
     }
 }
